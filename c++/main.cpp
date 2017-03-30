078db6942f81bad099d07f846bc60a0291fbd9ea
Shem Hale <s.hale@aggiemail.usu.edu>
2017-03-29 19:19:12 -0600

    fix serial comm and add elapsed time

--- c++/main.cpp
+++ c++/main.cpp
@@ -44,6 +44,8 @@
 #include <iostream>
 #include <stdio.h>
 
+#include <chrono>
+
 using namespace std;
 using namespace cv;
 
@@ -54,8 +56,11 @@ using std::cerr;
 using std::endl;
 using std::vector;
 
+
 /** Global variables */
-String face_cascade_name = "/home/barbie/Documents/python/opencv/opencv/data/lbpcascades/lbpcascade_frontalface_improved.xml";
+//String face_cascade_name = "/home/barbie/Documents/python/opencv/opencv/data/haarcascades/haarcascade_frontalface_default.xml";
+String face_cascade_name = "/home/barbie/lbp_face.xml";
+//String face_cascade_name = "/home/barbie/Documents/python/opencv/opencv/data/lbpcascades/lbpcascade_frontalface_improved.xml";
 String eyes_cascade_name = "/home/barbie/Documents/python/opencv/opencv/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
 CascadeClassifier eyes_cascade;
@@ -64,6 +69,12 @@ String window_name = "Capture - Face detection";
 unsigned char LSB = 0;
 unsigned char MSB = 0;
 short MSBLSB = 0;
+
+auto start = std::chrono::steady_clock::now();
+// do something
+auto finish = std::chrono::steady_clock::now();
+double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double> >(finish - start).count();
+
 
 /** Function Headers */
 void detectAndDisplay( Mat frame );
@@ -74,6 +85,14 @@ void my_sleep(unsigned long milliseconds) {
 #else
      usleep(milliseconds*1000); // 100 ms
 #endif
+}
+
+vector<unsigned char> intToBytes(int paramInt)
+{
+     vector<unsigned char> arrayOfByte(4);
+     for (int i = 0; i < 4; i++)
+         arrayOfByte[3 - i] = (paramInt >> (i * 8));
+     return arrayOfByte;
 }
 
 void print_usage()
@@ -124,6 +143,7 @@ int main(int argc, char **argv)
     else
       cout << " No." << endl;
 
+    /*
     // Get the Test string
     int count = 0;
     string test_string;
@@ -132,6 +152,7 @@ int main(int argc, char **argv)
     } else {
       test_string = "Testing.";
     }
+    */
 
     
     VideoCapture capture;
@@ -144,6 +165,8 @@ int main(int argc, char **argv)
     //-- 2. Read the video stream
     capture.open( -1 );
     if ( ! capture.isOpened() ) { printf("--(!)Error opening video capture\n"); return -1; }
+
+    start = std::chrono::steady_clock::now();
 
     while ( capture.read(frame) )
     {
@@ -161,39 +184,78 @@ int main(int argc, char **argv)
         equalizeHist( frame_gray, frame_gray );
 
         //-- Detect faces
+        //face_cascade.detectMultiScale( frame_gray, faces, 1.4, 2, 0, Size(30, 30) );
         face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0, Size(80, 80) );
+
+        unsigned int x;
+        unsigned int y;
 
         for( size_t i = 0; i < faces.size(); i++ )
         {
-            LSB = (faces[i].x+faces[i].width/2) & 0xff;
-            MSB = ((faces[i].x+faces[i].width/2) >> 8) & 0xff;
+            /*LSB = (faces[i].x) & 0xff;
+            MSB = ((faces[i].x) >> 8) & 0xff;
             arduino.write(MSB+"");
             arduino.write(LSB+"");
-            LSB = (faces[i].y+faces[i].height/2) & 0xff;
-            MSB = ((faces[i].y+faces[i].height/2) >> 8) & 0xff;
+            LSB = (faces[i].y) & 0xff;
+            MSB = ((faces[i].y) >> 8) & 0xff;
             arduino.write(MSB+"");
             arduino.write(LSB+"");
-            
+
+            cout << "x: " << faces[i].x << endl;
+            */
+            x = faces[i].x;
+            y = faces[i].y;
+
+            /*
             Mat faceROI = frame_gray( faces[i] );
             std::vector<Rect> eyes;
-
+            
             //-- In each face, detect eyes
-            eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-            if( eyes.size() == 2)
+            //eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+            eyes_cascade.detectMultiScale( faceROI, eyes, 1.4, 3, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
+            //if( eyes.size() == 2)
             {
                 //-- Draw the face
                 Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
-                ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+                //ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+                rectangle( frame, Rect(faces[i].x,faces[i].y,faces[i].width,faces[i].height), Scalar( 255, 0, 0), 2, 8, 0 );
 
                 for( size_t j = 0; j < eyes.size(); j++ )
                 { //-- Draw the eyes
                     Point eye_center( faces[i].x + eyes[j].x + eyes[j].width/2, faces[i].y + eyes[j].y + eyes[j].height/2 );
                     int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-                    circle( frame, eye_center, radius, Scalar( 255, 0, 255 ), 3, 8, 0 );
+                    //circle( frame, eye_center, radius, Scalar( 255, 0, 255 ), 3, 8, 0 );
+                    rectangle( frame, Rect(faces[i].x+eyes[i].x,faces[i].y+eyes[i].y,eyes[i].width,eyes[i].height), Scalar( 0, 255, 0), 2, 8, 0 );
                 }
             }
+            */
+
+
+            //-- Draw the face
+            Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+            //ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 255, 0, 0 ), 2, 8, 0 );
+            rectangle( frame, Rect(faces[i].x,faces[i].y,faces[i].width,faces[i].height), Scalar( 255, 0, 0), 2, 8, 0 );
+            
 
         }
+        
+        finish = std::chrono::steady_clock::now();
+        elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double> >(finish - start).count();
+        
+        if (elapsed_seconds > 0.2) {
+            if ( (x > 0) && (x < 640) && (y > 0) && (y < 480) )
+            {
+                cout << "x: " << x;
+                arduino.write(std::to_string(x));
+                arduino.write("\n");
+                cout << "   y: " << y << endl;
+                arduino.write(std::to_string(y));
+                arduino.write("\n");
+            }
+            start = std::chrono::steady_clock::now();
+        }
+
+        
         //-- Show what you got
         imshow( window_name, frame );
 
